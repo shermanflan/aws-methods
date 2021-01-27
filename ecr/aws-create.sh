cddc9f4a1697cd3047132a3dd3fa7c13307aaec0
shermanflan <shermanflan@gmail.com>
2021-01-26 18:36:48 -0600

    Added delete script

--- ecr/aws-create.sh
+++ ecr/aws-create.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eux
 
-# echo "Creating repository [${REPO_NAME}]"
-# aws ecr create-repository \
-#     --repository-name ${REPO_NAME} \
-#     --image-scanning-configuration scanOnPush=true
+echo "Creating repository [${REPO_NAME}]"
+aws ecr create-repository \
+    --repository-name ${REPO_NAME} \
+    --image-scanning-configuration scanOnPush=true
     # --generate-cli-skeleton
 
 declare REGISTRY_ID=$(aws ecr describe-registry | jq -r .registryId)
@@ -12,18 +12,17 @@ echo "Authentiating Docker to [${REGISTRY_ID}]"
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com
 
 echo "Tagging image [${IMAGE}]"
-docker tag ${IMAGE}:${VERSION} ${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${IMAGE}:${VERSION}
+docker tag ${IMAGE}:${VERSION} ${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${VERSION}
 
-echo "Pusing image [${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${IMAGE}:${VERSION}]"
-docker push ${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${IMAGE}:${VERSION}
+echo "Pusing image [${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${VERSION}]"
+docker push ${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${VERSION}
 
 echo "Describe images"
 aws ecr describe-repositories
-aws ecr describe-images --repository-name ${IMAGE}
+aws ecr describe-images --repository-name ${REPO_NAME}
+aws ecr list-images \
+    --registry-id ${REGISTRY_ID} \
+    --repository-name ${REPO_NAME}
 
-# aws ecr list-images \
-#     --registry-id ${REGISTRY_ID} \
-#     --repository-name ${IMAGE}
-
-echo "Pulling image [${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${IMAGE}:${VERSION}]"
-docker pull ${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${IMAGE}:${VERSION}
+echo "Pulling image [${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${VERSION}]"
+docker pull ${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPO_NAME}:${VERSION}
---
+++ ecr/aws-delete.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+
+declare REGISTRY_ID=$(aws ecr describe-registry | jq -r .registryId)
+
+aws ecr delete-repository \
+    --registry-id ${REGISTRY_ID} \
+    --repository-name ${REPO_NAME} \
+    --force
