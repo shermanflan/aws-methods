362d33761e07f81d51fa1b108bd01d1273dbeab4
shermanflan <shermanflan@gmail.com>
2021-04-04 10:59:07 -0500

    End to end working config AZ and AWS

--- eks/eksctl-create.sh
+++ eks/eksctl-create.sh
@@ -8,21 +8,21 @@ echo "Creating cluster ${EKS_CLUSTER_NAME} in ${EKS_REGION}"
 
 # NOTE: EMR Studio does not currently support Amazon EMR on EKS when you use
 # an AWS Fargate-only Amazon EKS cluster.
-# eksctl create cluster \
-#     --name="${EKS_CLUSTER_NAME}" \
-#     --region="${EKS_REGION}" \
-#     --version="1.19" \
-#     --nodegroup-name="ng-1" \
-#     --managed \
-#     --instance-types="m5.xlarge" \
-#     --nodes=2 \
-#     --node-volume-size=40
+eksctl create cluster \
+    --name="${EKS_CLUSTER_NAME}" \
+    --region="${EKS_REGION}" \
+    --version="1.19" \
+    --nodegroup-name="ng-1" \
+    --managed \
+    --instance-types="m5.xlarge" \
+    --nodes=2 \
+    --node-volume-size=80
 
 #    --with-oidc
 
 # OR: Inspired by https://github.com/aws-samples/amazon-eks-apache-spark-etl-sample/blob/master/example/eksctl.yaml
-eksctl create cluster \
-    --config-file=./cluster_config/spark-cluster.yaml
+# eksctl create cluster \
+#     --config-file=./cluster_config/spark-cluster.yaml
 
 # Enable Cloudwatch
 # eksctl utils update-cluster-logging \
